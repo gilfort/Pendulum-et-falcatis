aa65ec3c3570f8d2537333a0239c3b22bdbf5ecc
gilfort <jan.gilfort@gmx.de>
2018-06-21 17:36:47 +0200

    Initial commit

---
+++ src/main/java/de/gilfort/PendulumEtFalcatis/ExampleMod.java
@@ -0,0 +1,35 @@
+package de.gilfort.PendulumEtFalcatis;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION)
+public class ExampleMod
+{
+
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+       
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	
+    }
+    
+}
---
+++ src/main/java/de/gilfort/PendulumEtFalcatis/References.java
@@ -0,0 +1,10 @@
+package de.gilfort.PendulumEtFalcatis;
+
+public class References {
+
+	public static final String VERSION = "1.0";
+	public static final String MODID = "pendulumetfalcatis";
+	public static final String NAME = "Pendulum et Falcatis";
+	
+	
+}
